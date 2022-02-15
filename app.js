49d7afae1ce525f4fbf5504792ab3bb69d27331e
Lee Siang Yin <siangyin916@gmail.com>
2022-02-15 17:53:31 +0800

    ✅ Basic express setup, middleware & err handlers

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require("./db/connectDB");
+const morgan = require("morgan");
 
+// MIDDLEWARE
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(morgan("tiny")); // production only
+app.use(express.json());
+
+// ROUTES
 app.get("/", (req, res) => {
-	res.send("Hello");
+	res.send("Hello Greenlab");
 });
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
 	try {
---
+++ errors/index.js
@@ -0,0 +1,11 @@
+const CustomAPIError = require("./CustomAPIError");
+const UnauthenticatedError = require("./UnauthenticatedError");
+const NotFoundError = require("./NotFoundError");
+const BadRequestError = require("./BadRequestError");
+
+module.exports = {
+	CustomAPIError,
+	UnauthenticatedError,
+	NotFoundError,
+	BadRequestError,
+};
