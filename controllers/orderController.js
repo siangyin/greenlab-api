3c73c1f9432b12e3e7345f334531449051b09614
Lee Siang Yin <siangyin916@gmail.com>
2022-02-20 01:21:15 +0800

    ✅ orderControllers complete

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -65,22 +65,44 @@ const createOrder = async (req, res) => {
 
 // GET ALL ORDERS
 const getAllOrders = async (req, res) => {
-	res.send("getAllOrders");
+	const orders = await Order.find({});
+	return res.status(StatusCodes.OK).json({ orders, count: orders.length });
 };
 
 // GET SINGLE ORDER
 const getSingleOrder = async (req, res) => {
-	res.send("getSingleOrder");
+	const { id: orderId } = req.params;
+	const order = await Order.findOne({ _id: orderId });
+	if (!order) {
+		throw new CustomError.NotFoundError(`No order with id : ${orderId}`);
+	}
+	checkPermissions(req.user, order.user);
+	console.log(req.user);
+	console.log(order.user);
+	res.status(StatusCodes.OK).json({ order });
 };
 
 // GET CURRENT USER ORDER
 const getCurrentUserOrders = async (req, res) => {
-	res.send("getCurrentUserOrders");
+	const orders = await Order.find({ user: req.user.userID });
+	res.status(StatusCodes.OK).json({ orders, count: orders.length });
 };
 
 // UPDATE ORDER
 const updateOrder = async (req, res) => {
-	res.send("updateOrder");
+	const { id: orderId } = req.params;
+
+	let order = await Order.findOne({ _id: orderId });
+	if (!order) {
+		throw new CustomError.NotFoundError(`No order with id : ${orderId}`);
+	}
+	checkPermissions(req.user, order.user);
+
+	order = await Order.findOneAndUpdate({ _id: orderId }, req.body, {
+		new: true,
+		runValidators: true,
+	});
+	res.status(StatusCodes.OK).json({ order });
 };
 
 module.exports = {
