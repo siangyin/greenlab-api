a82ad2b08fc48206a24de2772ec1a1eced86dc03
Lee Siang Yin <siangyin916@gmail.com>
2022-02-24 01:04:00 +0800

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,7 @@ const {
 const getAllUsers = async (req, res) => {
 	// console.log(req.user);
 	// { name: 'siangyin', userID: '6206712bc2255c8b3f6f946b', role: 'user' }
-	const users = await User.find({ role: "user" }).select("-password");
+	const users = await User.find({}).select("-password");
 	res.status(StatusCodes.OK).json({ status: "OK", data: users });
 };
 
